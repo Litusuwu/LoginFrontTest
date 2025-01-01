d80ce056d43289ef92777286e605f1b4a85121bf
Litusuwu <zegarraward@gmail.com>
2025-01-01 01:10:27 -0500

    testing

--- login-page/src/main.js
+++ login-page/src/main.js
@@ -1,5 +1,20 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// import './style.css'
+
+const input = document.getElementById('emailI');
+const label = document.getElementById('usuarioL')
+
+input.addEventListener('focus', ()=>{
+    label.style.color = '#00498b';
+    // label.style.transform = 'scale(1.1, 1.1)';
+    label.style.transform = 'translate(2px,-13px) scale(0.7)';
+    label.style.zIndex ='1111';
+    label.style.paddingRight = '0.5rem';
+    input.style.color = '#042354';
+});
+
+input.addEventListener('blur', ()=>{
+    label.style.color = 'black';
+    label.style.transform = 'none';
+    // label.style.backgroundColor = 'blue'; 
+});
 
