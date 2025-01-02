061d4482550b1bb351f95e4935ebad8dfd19bad7
Litusuwu <carlos.zegarrab@pucp.edu.pe>
2025-01-02 10:28:49 -0500

    hehe

--- login-page/src/main.js
+++ login-page/src/main.js
@@ -3,18 +3,26 @@
 const input = document.getElementById('emailI');
 const label = document.getElementById('usuarioL')
 
-input.addEventListener('focus', ()=>{
+function applyStylesInput(){
     label.style.color = '#00498b';
     // label.style.transform = 'scale(1.1, 1.1)';
     label.style.transform = 'translate(2px,-13px) scale(0.7)';
     label.style.zIndex ='1111';
     label.style.paddingRight = '0.5rem';
     input.style.color = '#042354';
+}
+input.addEventListener('focus', ()=>{
+    applyStylesInput();
+});
+input.addEventListener('input', ()=>{
+    applyStylesInput();
 });
 
 input.addEventListener('blur', ()=>{
-    label.style.color = 'black';
-    label.style.transform = 'none';
-    // label.style.backgroundColor = 'blue'; 
+    if(!input.target.is(':valid')){
+        label.style.color = 'black';
+        label.style.transform = 'none';
+        // label.style.backgroundColor = 'blue';    
+    }
 });
 
